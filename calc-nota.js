3264ecc7a9e7049332e502de6bf63a0a58136008
Albertina75 <145276835+Albertina75@users.noreply.github.com>
2023-10-16 21:54:26 +0200

    calc-notas completado

--- calc-nota.js
+++ calc-nota.js
@@ -1,9 +1,23 @@
 /** Pssst: Recuerda leer el fichero README.md */
 
+
+
 function calcNota(input, solution) {
-  return;
+  if (input.length !== solution.length) {
+    throw new Error ("Las respuestas del estudiante y la solución no tienen la misma longitud ");
+  }
+  let puntuacion = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === solution [i]) {
+      puntuacion += 1;
+    } else {
+      puntuacion -= 0.25;
+  }
+}puntuacion = Math.max(0, Math.min(100, puntuacion));
+
+  return puntuacion;
 }
 
 console.log(calcNota("ABCDBBACAD", "ABCDBBACAD")); // 100 (el examen está perfecto)
 console.log(calcNota("CBCDBBACAC", "ABCDBBACAD")); // 80 (hay 2 errores)
-console.log(calcNota("CCACCCAVV", "ABCDBBACAD")); // 0 (todo mal...)
+console.log(calcNota("CCACCCDVVA", "ABCDBBACAD")); // 0 (todo mal...)
